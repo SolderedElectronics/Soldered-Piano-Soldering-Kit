0541073de1bf0bf83b8353d45c6dd9b0a0d8900b
KarloLeksic <lekxka@gmail.com>
2023-03-02 13:14:20 +0100

    Add source code

---
+++ Piano_Soldering_Kit_Arduino_Code/Piano_Soldering_Kit_Arduino_Code.ino
@@ -0,0 +1,103 @@
+// Include a library for the capacitive touchpads
+#include "CapacitiveSensor.h"
+
+// Include a file that contains frequencies for each note
+#include "pitches.h"
+
+// Output pin connected to the speaker
+#define PWM_OUT_PIN 10
+
+// Pin of the switch for changing the octave
+#define OCTAVE_SWITCH_PIN A0
+
+// Number of keys
+#define NUM_KEYS 12
+
+// The threshold to recognizing key press
+#define KEY_PRESS_THRESHOLD 100
+
+// Set capacitive sensor sensitivity
+#define CAPACITIVE_SENSOR_SENSITIVITY 15
+
+// Pointers for each key object
+CapacitiveSensor *keys[NUM_KEYS];
+
+// Pins for piano keys
+const uint8_t keyPins[] = {A3, 0, 1, 2, 3, 4, A5, A4, 5, 6, 7, 8};
+
+// Common send pin for all keys
+const uint8_t sendPin = 9;
+
+
+void setup()
+{
+    // Go through each key
+    for (int i = 0; i < NUM_KEYS; i++)
+    {
+        // Create a key object
+        keys[i] = new CapacitiveSensor(sendPin, keyPins[i]);
+
+        // Turn off auto calibrate
+        keys[i]->set_CS_AutocaL_Millis(0xFFFFFFFF);
+    }
+}
+
+
+void loop()
+{
+    // Get selected octave
+    int currentOctave = getOctave();
+
+    // Go through each key
+    for (int i = 0; i < NUM_KEYS; i++)
+    {
+        // Check if the key is pressed
+        if (keys[i]->capacitiveSensor(CAPACITIVE_SENSOR_SENSITIVITY) > KEY_PRESS_THRESHOLD)
+        {
+            // Play the tone of the pressed key
+            tone(PWM_OUT_PIN, tones[currentOctave][i]);
+
+            // Because the tone function cannot reproduce more than 1 tone at the same time, no need to check other keys
+            break;
+        }
+    }
+
+    // Stop the tone only if the all keys are released
+    bool allKeysReleased = 1;
+
+    // Go through each key
+    for (int i = 0; i < NUM_KEYS; i++)
+    {
+        // Check if the key is pressed
+        if (keys[i]->capacitiveSensor(CAPACITIVE_SENSOR_SENSITIVITY) > KEY_PRESS_THRESHOLD)
+        {
+            // If the key is pressed, set the flag to 0, and don't check next
+            allKeysReleased = 0;         
+            break;
+        }
+    }
+
+    // If all of the keys are released
+    if (allKeysReleased)
+    {
+        // Stop the tone
+        noTone(PWM_OUT_PIN);
+    }
+}
+
+// Read the current octave from the switch and return the selected octave
+int getOctave()
+{
+    // Read the analog value
+    int analogSwitchRead = analogRead(OCTAVE_SWITCH_PIN);
+
+    // Return a value depending on the range it is in
+    if (analogSwitchRead < 100) // The real value is around 0
+        return 0;
+    else if (analogSwitchRead > 240 && analogSwitchRead < 440) // The real value is around 340
+        return 1;
+    else if (analogSwitchRead > 590 && analogSwitchRead < 790) // The real value is around 690
+        return 2;
+    else if (analogSwitchRead > 900) // The real value is around 1023
+        return 3;
+}
---
+++ Piano_Soldering_Kit_Arduino_Code/pitches.h
@@ -0,0 +1,58 @@
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+
+
+// 4 Octaves, 12 tones on each octave
+const int tones[4][12] = 
+{
+    {NOTE_C3, NOTE_CS3, NOTE_D3, NOTE_DS3, NOTE_E3, NOTE_F3, NOTE_FS3, NOTE_G3, NOTE_GS3, NOTE_A3, NOTE_AS3, NOTE_B3},
+    {NOTE_C4, NOTE_CS4, NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4, NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4},
+    {NOTE_C5, NOTE_CS5, NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5},
+    {NOTE_C6, NOTE_CS6, NOTE_D6, NOTE_DS6, NOTE_E6, NOTE_F6, NOTE_FS6, NOTE_G6, NOTE_GS6, NOTE_A6, NOTE_AS6, NOTE_B6}
+};
